5a8d8d418bd12c03c705c889d2f49a7cd5ba5b6b
amizony <amizony@gmail.com>
2015-07-14 11:52:51 +0200

    Base project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rpgApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rpgApp
+ */
+angular.module('rpgApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rpgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rpgApp
+ */
+angular.module('rpgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
