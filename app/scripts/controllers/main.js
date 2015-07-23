13e1ff4e3566bff90822f8a788397a2078c5cfce
amizony <amizony@gmail.com>
2015-07-23 19:14:28 +0200

    Added a random map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,27 +25,47 @@ angular.module('rpgApp')
       fpsPosition.innerHTML = fps;
     }, 1000);
 
+    var map;
+    function loadMap() {
+    	map = JSON.parse(data);
+    }
 
-
+    loadMap();
     var stage = new PIXI.Container();
     var renderer = PIXI.autoDetectRenderer(800, 600, {view:document.getElementById("game-canvas"), backgroundColor : 0x1099bb});
 
-    var texture = PIXI.Texture.fromImage("images/yeoman.png");
-    var yeo = new PIXI.Sprite(texture);
-    yeo.anchor.x = 0.5;
-    yeo.anchor.y = 0.5;
-    yeo.position.x = 400;
-    yeo.position.y = 300;
-    stage.addChild(yeo);
+    var groundTexture = PIXI.Texture.fromImage("images/ground.png");
+    var wallTexture = PIXI.Texture.fromImage("images/wall.png");
 
+    var posX = -16;
+    var posY = -16;
+
+    for (var line in map) {
+      posY += 32;
+      posX = -16;
+      for (var i = 0; i < map[line].length; i++) {
+        posX += 32;
+        map[line][i] = _.random(2);
+        if (map[line][i] == 0) {
+          createSquare(posX, posY, wallTexture);
+        } else {
+          createSquare(posX, posY, groundTexture);
+        }
+      }
+    }
+
+    function createSquare(x, y, texture) {
+      var square = new PIXI.Sprite(texture);
+      square.anchor.set(0.5);
+      square.scale.set(0.125);
+      square.position.x = x;
+      square.position.y = y;
+      stage.addChild(square);
+    }
 
 
     function animate() {
       requestAnimationFrame(animate);
-      var posX = _.random(-5, 5);
-      var posY = _.random(-5, 5);
-      yeo.position.x += posX;
-      yeo.position.y += posY;
       renderer.render(stage);
       frames += 1;
     }
