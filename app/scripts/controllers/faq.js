63509643461cd7e9d18d13fc499a1f1c5d55ef62
amizony <amizony@gmail.com>
2015-07-23 11:38:16 +0200

    Initializing rendering with PIXI

---
+++ app/scripts/controllers/faq.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rpgApp.controller:FaqCtrl
+ * @description
+ * # FaqCtrl
+ * Controller of the rpgApp
+ */
+angular.module('rpgApp')
+  .controller('FaqCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,46 @@ angular.module('rpgApp')
       'AngularJS',
       'Karma'
     ];
+
+    var frames = 0;
+    var fps = 0;
+
+    window.setInterval( function() {
+      var fpsPosition = document.getElementById("fps-div");
+      fps = "fps: " + frames;
+      frames = 0;
+      fpsPosition.innerHTML = fps;
+    }, 1000);
+
+
+
+    var stage = new PIXI.Container();
+    var renderer = PIXI.autoDetectRenderer(800, 600, {view:document.getElementById("game-canvas"), backgroundColor : 0x1099bb});
+
+    var texture = PIXI.Texture.fromImage("images/yeoman.png");
+    var yeo = new PIXI.Sprite(texture);
+    yeo.anchor.x = 0.5;
+    yeo.anchor.y = 0.5;
+    yeo.position.x = 400;
+    yeo.position.y = 300;
+    stage.addChild(yeo);
+
+
+    animate();
+    function run(callback) {
+      requestAnimationFrame(callback);
+      frames += 1;
+    }
+
+
+    function animate() {
+      requestAnimationFrame(animate);
+      var posX = _.random(-5, 5);
+      var posY = _.random(-5, 5);
+      yeo.position.x += posX;
+      yeo.position.y += posY;
+      renderer.render(stage);
+      frames += 1;
+    }
+
   });
