caed27a1ae65d0a269a3fc4cc38396edf97a3d9b
amizony <amizony@gmail.com>
2015-07-23 22:00:16 +0200

    Updated the map as json file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -26,33 +26,33 @@ angular.module('rpgApp')
     }, 1000);
 
     var map;
-    function loadMap() {
-    	map = JSON.parse(data);
-    }
 
-    loadMap();
+
     var stage = new PIXI.Container();
     var renderer = PIXI.autoDetectRenderer(800, 600, {view:document.getElementById("game-canvas"), backgroundColor : 0x1099bb});
 
     var groundTexture = PIXI.Texture.fromImage("images/ground.png");
     var wallTexture = PIXI.Texture.fromImage("images/wall.png");
 
-    var posX = -16;
-    var posY = -16;
+    $.getJSON("resources/map.json", function(data) {
+       map = data;
+       var posX = -16;
+       var posY = -16;
 
-    for (var line in map) {
-      posY += 32;
-      posX = -16;
-      for (var i = 0; i < map[line].length; i++) {
-        posX += 32;
-        map[line][i] = _.random(2);
-        if (map[line][i] == 0) {
-          createSquare(posX, posY, wallTexture);
-        } else {
-          createSquare(posX, posY, groundTexture);
-        }
-      }
-    }
+       for (var line in map) {
+         posY += 32;
+         posX = -16;
+         for (var i = 0; i < map[line].length; i++) {
+           posX += 32;
+           map[line][i] = _.random(2);
+           if (map[line][i] === 0) {
+             createSquare(posX, posY, wallTexture);
+           } else {
+             createSquare(posX, posY, groundTexture);
+           }
+         }
+       }
+    });
 
     function createSquare(x, y, texture) {
       var square = new PIXI.Sprite(texture);
